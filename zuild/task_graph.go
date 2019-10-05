725936b4f622b69ef087519b79d1e91fa86e5c8b
Zachary Schneider <zachary.schneider@instana.com>
2019-10-04 22:44:54 -0500

    stable sort graph nodes by req appearance

--- zuild/task_graph.go
+++ zuild/task_graph.go
@@ -4,11 +4,14 @@ import (
 	"gonum.org/v1/gonum/graph"
 	"gonum.org/v1/gonum/graph/simple"
 	"gonum.org/v1/gonum/graph/topo"
+	"sort"
 )
 
 type TaskGraph struct {
 	graph     *simple.DirectedGraph
 	root      graph.Node
+
+	docIndex  map[graph.Node]int
 	nodeIndex map[graph.Node]*Task
 	nameIndex map[string]*Task
 }
@@ -18,6 +21,8 @@ func NewTaskGraph() *TaskGraph {
 
 	tg.graph = simple.NewDirectedGraph()
 	tg.root = tg.graph.NewNode()
+
+	tg.docIndex = make(map[graph.Node]int)
 	tg.nodeIndex = make(map[graph.Node]*Task)
 	tg.nameIndex = make(map[string]*Task)
 
@@ -37,7 +42,7 @@ func (t *TaskGraph) Populate(tasks []*Task) {
 func (t *TaskGraph) Get(root string) ([]*Task, error) {
 	var tasks []*Task
 
-	nodes, err := topo.Sort(t.graph)
+	nodes, err := topo.SortStabilized(t.graph, t.sortStable)
 	if err != nil {
 		return nil, err
 	}
@@ -54,6 +59,7 @@ func (t *TaskGraph) Get(root string) ([]*Task, error) {
 func (t *TaskGraph) addNode(task *Task) {
 	task.Node = t.graph.NewNode()
 	t.graph.AddNode(task.Node)
+
 	t.nodeIndex[task.Node] = task
 	t.nameIndex[task.Name] = task
 }
@@ -62,5 +68,13 @@ func (t *TaskGraph) addEdges(task *Task) {
 	for _, req := range task.Require {
 		edge := t.graph.NewEdge(t.nameIndex[req].Node, task.Node)
 		t.graph.SetEdge(edge)
+
+		t.docIndex[t.nameIndex[req].Node] = len(t.docIndex)
 	}
+}
+
+func (t *TaskGraph) sortStable(nodes []graph.Node) {
+	sort.SliceStable(nodes, func(i, j int) bool {
+		return t.docIndex[nodes[i]] <  t.docIndex[nodes[j]]
+	})
 }
